49b72b0bf1e4ab3aaecfbb7ef22736e99c6ce100
Jack Greenberg <jagreenberg@yext.com>
2022-07-29 14:00:24 -0400

    Adding reply and other tweaks

--- main.go
+++ main.go
@@ -11,23 +11,41 @@ func main() {
 	var input struct {
 		MessageRef string
 	}
-	err := json.NewDecoder(strings.NewReader(os.Args[0])).Decode(&input)
+	err := json.NewDecoder(strings.NewReader(os.Args[1])).Decode(&input)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	output := struct {
+	react := struct {
 		MessageRef string
 		Content    string
+		Action     string
 	}{
 		MessageRef: input.MessageRef,
 		Content:    "+1",
+		Action:     "react",
 	}
 
-	err = json.NewEncoder(os.Stdout).Encode(&output)
+	reply := struct {
+		MessageRef string
+		Content    string
+		Action     string
+	}{
+		MessageRef: input.MessageRef,
+		Content:    ":+1:",
+		Action:     "reply",
+	}
+
+	b, err := json.Marshal(&react)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	b2, err := json.Marshal(&reply)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf("#!#%s\n#!#%s", string(b), string(b2))
 }
