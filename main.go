2e65a7bac04de60c4bc84d76443c976e70575963
Jack Greenberg <jagreenberg@yext.com>
2022-08-11 15:38:35 -0400

    update

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ import (
 func main() {
 	var input struct {
 		MessageRef string
+		Channel    struct {
+			ID string
+		}
 	}
 	err := json.NewDecoder(strings.NewReader(os.Args[1])).Decode(&input)
 	if err != nil {
@@ -37,6 +40,18 @@ func main() {
 		Action:     "reply",
 	}
 
+	bookmark := struct {
+		Title     string
+		Content   string
+		Action    string
+		ChannelID string
+	}{
+		Title:     "test",
+		Content:   "https://jackg.dev",
+		Action:    "bookmark",
+		ChannelID: input.Channel.ID,
+	}
+
 	b, err := json.Marshal(&react)
 	if err != nil {
 		fmt.Println(err)
@@ -47,5 +62,11 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Printf("#!#%s\n#!#%s", string(b), string(b2))
+	b3, err := json.Marshal(&bookmark)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("#!#%s\n#!#%s\n#!#%s", string(b), string(b2), string(b3))
 }
